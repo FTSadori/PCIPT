8df2dd2c9d1adbf092de20e93fcf6d29b81503b9
В'ячеслав <suzium.trap@gmail.com>
2024-09-03 12:57:36 +0300

    Added VehicleByRoutesCalculator

---
+++ PCIPT/Calculations/FirstStage/VehicleInRoute/Dtos/VehicleInRouteStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCIPT.Calculations.FirstStage.VehicleInRoute.Dtos
+{
+    public sealed record VehicleInRouteStats(string Name,
+        float TransportCycleSize,
+        float NumberOfTransportCyclesPerDay,
+        float RoutePerformance,
+        float AverageDailyCargoVolume,
+        float TheRequiredNumberOfVehicles,
+        float TotalCost,
+        float TotalNumberOfCycles,
+        float TotalTimeDailyNeeded);
+}
---
+++ PCIPT/Calculations/FirstStage/VehicleInRoute/VehicleInRouteStatsCalculator.cs
@@ -0,0 +1,63 @@
+﻿using CsvHelper.Configuration.Attributes;
+using PCIPT.Calculations.FirstStage.CostByVehicle.Dtos;
+using PCIPT.Calculations.FirstStage.VehicleInRoute.Dtos;
+using PCIPT.Dtos.Cargoes;
+using PCIPT.Dtos.CargoTurnoverPoints;
+using PCIPT.Dtos.Routes;
+using PCIPT.Dtos.Vehicles;
+using PCIPT.Dtos.VehicleTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace PCIPT.Calculations.FirstStage.VehicleInRoute
+{
+    public sealed class VehicleInRouteStatsCalculator
+    {
+        private static float CalculateDailyCargoTurnover(float thisCargoTurnover, float fullCargoTurnover, float workingDays, float maxDailyCargo)
+        {
+            float Qav = fullCargoTurnover / workingDays;
+            float K = maxDailyCargo / Qav;
+            float Qdaily = thisCargoTurnover / workingDays * K;
+            return Qdaily;
+        }
+
+        public static List<VehicleInRouteStats> CalculateVehicleStats(List<VehicleDto> vehicles, List<VehicleTypeDto> vehicleTypes, 
+            List<CostTableRowEntity> costTable,
+            CargoTurnoverPointDto point, CargoDto cargo, float distance,
+            float dailyTimeFund, float fullCargoTurnover, float workingDays, float maxDailyCargo)
+        {
+            float Qdaily = CalculateDailyCargoTurnover(point.OutgoingCargo, fullCargoTurnover, workingDays, maxDailyCargo);
+
+            var vehicleTable = new List<VehicleInRouteStats>();
+
+            foreach (var vehicle in vehicles)
+            {
+                var types = vehicleTypes.Find(t => t.VehicleType == vehicle.Type && t.CargoType == cargo.Type);
+                if (types == null) 
+                    continue;
+
+                float Tc1 = distance * 60f / (vehicle.SpeedWithLoad * 1000f) + distance * 60f / (vehicle.SpeedWithoutLoad * 1000f) + 2f * vehicle.LoadTime;
+                //float Tc2 = 2f * (distance * 60f / (vehicle.SpeedWithLoad * 1000f)) + 4f * vehicle.LoadTime;
+                float mdaily = dailyTimeFund / Tc1;
+                float q = vehicle.LoadCapacity * cargo.CapacityUtilisationRate;
+                float qdaily = q * mdaily;
+                float N = Qdaily / qdaily;
+
+                float cost = costTable.Find(c => c.Name == vehicle.Name)?.TotalLoss ?? -1f;
+                if (cost < 0.0001f)
+                    continue;
+
+                float mtotal = mdaily * N;
+                float ttotal = Tc1 * mtotal;
+
+                vehicleTable.Add(new VehicleInRouteStats(vehicle.Name, Tc1, mdaily, q, qdaily, N, N * cost, mtotal, ttotal));
+            }
+
+            return vehicleTable;
+        }
+    }
+}
