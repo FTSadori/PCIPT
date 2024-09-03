d391248bea635cb47395edb67f3ca6a276f246ca
В'ячеслав <suzium.trap@gmail.com>
2024-09-03 12:57:19 +0300

    Added CargoTurnoverCalculator

---
+++ PCIPT/Calculations/FirstStage/CargoStats/CargoTurnoverCalculator.cs
@@ -0,0 +1,27 @@
+﻿using PCIPT.Dtos.CargoTurnoverPoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCIPT.Calculations.FirstStage.CargoStats
+{
+    public sealed class CargoTurnoverCalculator
+    {
+        public static Dictionary<int, float> GetTotalMassForEachCargo(List<CargoTurnoverPointDto> points)
+        {
+            var totalMasses = new Dictionary<int, float>();
+
+            foreach (var point in points)
+            {
+                if (totalMasses.ContainsKey(point.CargoCode))
+                    totalMasses[point.CargoCode] += point.OutgoingCargo;
+                else
+                    totalMasses[point.CargoCode] = point.OutgoingCargo;
+            }
+
+            return totalMasses;
+        }
+    }
+}
---
+++ PCIPT/Calculations/FirstStage/CargoStats/Dtos/CargoMass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCIPT.Calculations.FirstStage.CargoStats.Dtos
+{
+    public sealed record CargoMass(int CargoCode, float TotalMass);
+}
