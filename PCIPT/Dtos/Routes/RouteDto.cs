09e20b463114e234c10d4e9f67cedc2c09de50f1
В'ячеслав <suzium.trap@gmail.com>
2024-09-04 19:06:27 +0300

    Added Id field to Point and Route dtos

--- PCIPT/Dtos/Routes/RouteDto.cs
+++ PCIPT/Dtos/Routes/RouteDto.cs
@@ -6,5 +6,5 @@ using System.Threading.Tasks;
 
 namespace PCIPT.Dtos.Routes
 {
-    public sealed record RouteDto(string SourceName, string DestinationName, float Distance) : IData;
+    public sealed record RouteDto(int Id, string SourceName, string DestinationName, float Distance) : IData;
 }
